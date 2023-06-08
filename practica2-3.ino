ccdb541d3e28074c4dc695117b4648e3285127da
Christian  Solorzano <128552047+csolorzano-2022323@users.noreply.github.com>
2023-06-08 16:41:03 -0600

    Add files via upload

---
+++ practica2-3.ino
@@ -0,0 +1,23 @@
+/*
+   Fundacion KinaL
+   Centro educativo tecnico LaboraL KinaL
+   Quinto perito
+   Quinto eLectronica
+   Codigo Tecnico: EB5AM / EB5AV
+   Curso: TaLLer de eLectronica digitaL y reparacion de computadoras I
+   Proyecto: Control de una secuencia de colores RGB
+   aLumno:Christian SoLorzano
+   Fecha: 8/06/2023
+*/
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
