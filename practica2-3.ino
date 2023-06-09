3c4012f30d204ef6baa2d9949d9b4b4c13a79811
Christian  Solorzano <128552047+csolorzano-2022323@users.noreply.github.com>
2023-06-09 16:10:49 -0600

    Add files via upload

--- practica2-3.ino
+++ practica2-3.ino
@@ -1,3 +1,5 @@
+#include <LiquidCrystal_I2C.h>
+
 /*
    Fundacion KinaL
    Centro educativo tecnico LaboraL KinaL
@@ -5,19 +7,34 @@
    Quinto eLectronica
    Codigo Tecnico: EB5AM / EB5AV
    Curso: TaLLer de eLectronica digitaL y reparacion de computadoras I
-   Proyecto: Control de una secuencia de colores RGB
+   Proyecto: OHMIMETRO
    aLumno:Christian SoLorzano
    Fecha: 8/06/2023
 */
-
-
+LiquidCrystal_I2C LCD(0x27, 16, 2);
 
 void setup() {
-  // put your setup code here, to run once:
-
+  LCD.init();
+  LCD.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  float R2 = 10000;
+  float Vout = analogRead(A0);
+  float Vout2= map(Vout,0,1023,0,500000);
+  LCD.setCursor(0,1);
+  float Vout3= Vout2/100000;
+  float V1 = (R2*5) / Vout3;
+  float R1 = V1 - R2;
+  R1=R1+9;
+  LCD.setCursor(0, 0);
+  LCD.print("R1=");
+  LCD.setCursor(3, 0);
+  LCD.print(R1);
+  
+
+
+
 
 }
