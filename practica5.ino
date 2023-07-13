c519f0c871d23a0b59a6aa31a82675f436eb232b
Christian  Solorzano <128552047+csolorzano-2022323@users.noreply.github.com>
2023-07-13 14:32:02 -0600

    Update practica5.ino

--- practica5.ino
+++ practica5.ino
@@ -9,18 +9,15 @@
    aLumno:Christian SoLorzano
    Fecha: 26/06/2023
 */
-
-int Estado = 2;
-
+#include <Ticker.h>
 #include <LedControl.h>   //libreria que me permite usar el controlador led max7219
 #define dataPin0   12   //pin 12 conectado al pin de datos del max7219.
 #define dataClock0 11   //pin 11 conectado al pin de clock del max7219.
 #define dataLoad0  10   //pin 10 conectado al pin de carga del max7219.
 
-//LedControl matrixF = LedControl(dataPin0,dataClock0,dataLoad0,1);
-LedControl matrixF = LedControl(dataPin0,dataClock0,dataLoad0,1);
-byte FUEGO_0[8]= {        
-  B00000000,        
+LedControl matrixF = LedControl(dataPin0, dataClock0, dataLoad0, 1);
+byte FUEGO_0[8] = {
+  B00000000,
   B00001100,
   B00101100,
   B00111110,
@@ -29,8 +26,8 @@ byte FUEGO_0[8]= {
   B11111111,
   B11111111
 };
-byte  FUEGO_1[8]= {       
-  B00000000,        
+byte  FUEGO_1[8] = {
+  B00000000,
   B00000000,
   B00001000,
   B00101100,
@@ -40,8 +37,8 @@ byte  FUEGO_1[8]= {
   B11111111
 };
 
-byte  FUEGO_2[8]= {       
-  B00000000,     
+byte  FUEGO_2[8] = {
+  B00000000,
   B00000000,
   B00000000,
   B00000000,
@@ -51,8 +48,8 @@ byte  FUEGO_2[8]= {
   B11111111
 };
 
-byte  FUEGO_3[8]= {       
-  B00000000,        
+byte  FUEGO_3[8] = {
+  B00000000,
   B00001100,
   B00101100,
   B00111110,
@@ -62,8 +59,8 @@ byte  FUEGO_3[8]= {
   B11111111
 };
 
-byte  HIELO_0[8]= {     
-  B00011000,        
+byte  HIELO_0[8] = {
+  B00011000,
   B00111100,
   B01111110,
   B11111111,
@@ -72,70 +69,85 @@ byte  HIELO_0[8]= {
   B00011000,
   B00000000
 };
-byte  HIELO_2[8]= {     
-  B10101010,        
-  B10010011,
-  B01010100,
-  B01010001,
-  B00101000,
-  B01010100,
-  B10010011,
-  B10101010
+byte  HIELO_2[8] = {
+  B10011001,
+  B01011010,
+  B00111100,
+  B11100111,
+  B11100111,
+  B00111100,
+  B01011010,
+  B10011001
 };
 
 
 
 int i;
+int MQ, conver;
 unsigned long update_delay = 850;
 
-int display_number(unsigned char number);
-
+void medicion(void);
+Ticker medicionT(medicion, 2000);
 void setup() {
   Serial.begin(9600);
-  pinMode(Estado, INPUT);
-  matrixF.shutdown(0,false); //Activo el max7219 para poder mostrar los digitos.
-  matrixF.setIntensity(0,15);  //Brilo a la mitad 
+  matrixF.shutdown(0, false); //Activo el max7219 para poder mostrar los digitos.
+  matrixF.setIntensity(0, 15); //Brilo a la mitad
   matrixF.clearDisplay(0);    //limpio el display
   Serial.println("Medidor de fuego y matrix led");
+  medicionT.start();
 }
+
 void loop() {
+   medicionT.update();
+   if (conver < 40) {
 
-  boolean MQE = digitalRead(Estado); //LEE EL SENSOR
-
-  if (MQE)
-  {
     Serial.println("Sin presencia de fuego");
-    
-    for(int i=0; i<8;i++)
-      {
-        matrixF.setRow(0,i,HIELO_2[i]);
-      }
-      delay(500);
-      matrixF.clearDisplay(0);
+    //nieve
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, HIELO_2[i]);
+      
+    }
+    delay(500);
+     matrixF.clearDisplay(0);
   }
-
-  else {
+  if (conver >40 ) {
     Serial.println("Fuego detectado");
+    //fuego
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, FUEGO_0[i]);
+    }
+    delay(1000);
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, FUEGO_1[i]);
+    }
+    delay(1000);
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, FUEGO_2[i]);
+    }
+    delay(1000);
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, FUEGO_3[i]);
+    }
+    delay(1000);
+    for (int i = 0; i < 8; i++)
+    {
+      matrixF.setRow(0, i, FUEGO_2[i]);
+    }
+    delay(1000);    
+  }
     
-      for(int i=0; i<8;i++)
-      {
-        matrixF.setRow(0,i,FUEGO_0[i]);
-      }
-      delay(500);
-      for(int i=0; i<8;i++)
-      {
-        matrixF.setRow(0,i,FUEGO_1[i]);
-      }
-      delay(500);
-      for(int i=0; i<8;i++)
-      {
-        matrixF.setRow(0,i,FUEGO_2[i]);
-      }
-      delay(500);
 }
 
+void medicion(void) {
+
+  MQ = analogRead(A0);
+  conver = map(MQ, 0, 1023, 0, 100);
+  Serial.println(conver);  
+}
 }
 
-int display_number(unsigned char number)
-{
-}
